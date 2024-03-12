84cb68bcce80b7a323b32af7ca29ea474bb983ed
pankaj sondhiya <pankajsondhiya780@gamil.com>
2024-03-12 17:43:53 +0530

    fourth commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
-
+const cors = require("cors");
 const mongoose = require("mongoose");
 const express = require("express");
 const bodyParser = require("body-parser");
 const { DB_URL } = require("./configs/db.config");
 const { PORT } = require("./configs/server.config");
-const cors = require("cors");
+
 const app = express();
 
 app.use(cors());
