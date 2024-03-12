8a3a780faf4e151d2b3e3c6d15d237c18dd69023
pankaj sondhiya <pankajsondhiya780@gamil.com>
2024-03-12 20:00:31 +0530

    cors4

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,7 +3,7 @@ const { signIn, signUp } = require("../controllers/auth.controller");
 const { verifyToken } = require("../middleware/auth.jwt");
 
 module.exports = function (app) {
-  app.post("/fitsy/api/v1/auth/signin", [verifyToken], signIn);
-  app.post("/fitsy/api/v1/auth/signup", [verifyToken], signUp);
+  app.post("/fitsy/api/v1/auth/signin", signIn);
+  app.post("/fitsy/api/v1/auth/signup", signUp);
   // app.post("/fitsy/api/v1/auth/signin/otp", signInWithOTP") i
 };
